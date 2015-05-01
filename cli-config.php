21d9bb891003943945e12761b2597044fe4d8bd5
Carsten Bleicker <carsten@bleicker.de>
2015-05-01 16:36:53 +0200

    Fix cache paths

--- cli-config.php
+++ cli-config.php
@@ -5,6 +5,7 @@ use Bleicker\Persistence\EntityManagerInterface;
 
 include __DIR__ . '/vendor/autoload.php';
 include __DIR__ . '/src/Configuration/Essentials/Secrets.php';
+include __DIR__ . '/src/Configuration/Cache.php';
 include __DIR__ . '/src/Configuration/Persistence.php';
 
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet(ObjectManager::get(EntityManagerInterface::class));
--- src/Configuration/Persistence.php
+++ src/Configuration/Persistence.php
@@ -12,13 +12,9 @@ use Doctrine\ORM\Tools\Setup;
 Registry::set('doctrine.schema.paths.nodes', realpath(__DIR__ . "/../../vendor/bleicker/nodes/src/Schema/Persistence"));
 Registry::set('doctrine.schema.paths.nodestypes', realpath(__DIR__ . "/../../vendor/bleicker/nodetypes/src/Schema/Persistence"));
 
-ObjectManager::register(Cache::class, function () {
-	return new CacheImplementation(realpath(__DIR__ . '/../../cache/Doctrine'));
-});
-
 ObjectManager::register(EntityManagerInterface::class, function () {
 	return EntityManager::create(
 		Registry::get('DbConnection'),
-		Setup::createYAMLMetadataConfiguration(Registry::get('doctrine.schema.paths'), !Context::isProduction(), realpath(__DIR__ . '/../../cache/Proxies'), ObjectManager::get(Cache::class))
+		Setup::createYAMLMetadataConfiguration(Registry::get('doctrine.schema.paths'), !Context::isProduction(), __DIR__ . '/../../cache/doctrine', ObjectManager::get(Cache::class))
 	);
 });
