3a374a67430d3d64717ad136bee0ccc18e2dc7c9
Carsten Bleicker <carsten@bleicker.de>
2015-05-02 20:20:47 +0200

    Adjust Templates

--- src/ViewHelpers/Menus/PagetreeViewHelper.php
+++ src/ViewHelpers/Menus/PagetreeViewHelper.php
@@ -3,14 +3,16 @@
 namespace Bleicker\Cms\ViewHelpers\Menus;
 
 use Bleicker\Nodes\NodeInterface;
+use Bleicker\Nodes\NodeServiceInterface;
+use Bleicker\ObjectManager\ObjectManager;
 use TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper;
 
 /**
- * Class PagetreeViewHelper
+ * Class PageTreeViewHelper
  *
  * @package Bleicker\Cms\ViewHelpers\Menus
  */
-class PagetreeViewHelper extends AbstractViewHelper {
+class PageTreeViewHelper extends AbstractViewHelper {
 
 	/**
 	 * @var boolean
@@ -18,50 +20,12 @@ class PagetreeViewHelper extends AbstractViewHelper {
 	protected $escapeOutput = FALSE;
 
 	/**
-	 * Validate arguments, and throw exception if arguments do not validate.
-	 *
-	 * @return void
-	 * @throws \InvalidArgumentException
+	 * @var NodeServiceInterface
 	 */
-	public function __validateArguments() {
-		$argumentDefinitions = $this->prepareArguments();
-		foreach ($argumentDefinitions as $argumentName => $registeredArgument) {
-			if ($this->hasArgument($argumentName)) {
-				$value = $this->arguments[$argumentName];
-				$type = $registeredArgument->getType();
-				if ($value !== $registeredArgument->getDefaultValue() && $type !== 'mixed') {
-					$errorException = $this->createAnInvalidArgumentException($argumentName, $type, $value);
-					if ($type === 'array' && !is_array($value)) {
-						if (!$value instanceof \ArrayAccess && !$value instanceof \Traversable) {
-							throw $errorException;
-						}
-					} elseif ($type === 'string') {
-						if (is_object($value) && !method_exists($value, '__toString')) {
-							throw $errorException;
-						}
-					} elseif ($type === 'boolean' && !is_bool($value)) {
-						throw $errorException;
-					} elseif (is_object($value) && !($value instanceof $type)) {
-						throw $errorException;
-					}
-				}
-			}
-		}
-	}
+	protected $nodeService;
 
-	/**
-	 * @param string $argumentName
-	 * @param string $type
-	 * @return \InvalidArgumentException
-	 */
-	private function __createAnInvalidArgumentException($argumentName, $type) {
-		$value = $this->arguments[$argumentName];
-		$givenType = is_object($value) ? get_class($value) : gettype($value);
-		return new \InvalidArgumentException(
-			'The argument "' . $argumentName . '" was registered with type "' . $type . '", but is of type "' .
-			$givenType . '" in view helper "' . get_class($this) . '".',
-			1430483390
-		);
+	public function __construct() {
+		$this->nodeService = ObjectManager::get(NodeServiceInterface::class);
 	}
 
 	/**
@@ -72,7 +36,7 @@ class PagetreeViewHelper extends AbstractViewHelper {
 	 */
 	public function initializeArguments() {
 		$this->registerArgument('of', 'mixed', 'The start page', TRUE);
-		$this->registerArgument('as', 'array', 'The name of children variable', FALSE, 'children');
+		$this->registerArgument('as', 'string', 'The name of children variable', FALSE, 'children');
 	}
 
 	/**
@@ -85,7 +49,7 @@ class PagetreeViewHelper extends AbstractViewHelper {
 		/** @var string $childrenVariableName */
 		$childrenVariableName = $this->arguments['as'];
 
-		$children = $buildMenuOf->getChildren();
+		$children = $this->nodeService->getPages($buildMenuOf);
 
 		$this->templateVariableContainer->add($childrenVariableName, $children);
 		$content = $this->renderChildren();
