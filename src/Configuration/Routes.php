f91c89840414c31bb204e952491f1208f5d35523
Carsten Bleicker <carsten@bleicker.de>
2015-05-02 20:19:20 +0200

    Change route to nodemanager

--- src/Configuration/Routes.php
+++ src/Configuration/Routes.php
@@ -8,6 +8,5 @@ use Bleicker\Routing\RouterInterface;
 /** @var RouterInterface $router */
 $router = ObjectManager::get(RouterInterface::class);
 $router
-	->addRoute('/manager', 'get', new ControllerRouteData(NodeController::class, 'indexAction'))
-	->addRoute('/manager/{node}', 'get', new ControllerRouteData(NodeController::class, 'indexAction'));
-
+	->addRoute('/nodemanager', 'get', new ControllerRouteData(NodeController::class, 'indexAction'))
+	->addRoute('/nodemanager/{node}', 'get', new ControllerRouteData(NodeController::class, 'indexAction'));
