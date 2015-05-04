7188c426af4eac0f4c8ccee8eb4f731a978e5eec
Carsten Bleicker <carsten@bleicker.de>
2015-05-04 21:12:38 +0200

    Exclude sites from RegisteredNodeTypesViewHelper

--- src/ViewHelpers/RegisteredNodeTypesViewHelper.php
+++ src/ViewHelpers/RegisteredNodeTypesViewHelper.php
@@ -2,6 +2,8 @@
 
 namespace Bleicker\Cms\ViewHelpers;
 
+use Bleicker\Framework\Utility\Arrays;
+use Bleicker\NodeTypes\SiteInterface;
 use Bleicker\Registry\Registry;
 use TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper;
 
@@ -13,10 +15,24 @@ use TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper;
 class RegisteredNodeTypesViewHelper extends AbstractViewHelper {
 
 	/**
+	 * Returns all known type excluding sites
+	 *
 	 * @return array
 	 */
 	public function render() {
 		$registeredNodeTypes = Registry::get('nodetypes');
+		foreach ($registeredNodeTypes as $index => $registeredNodeType) {
+			$className = Arrays::getValueByPath($registeredNodeType, 'class');
+			if ($className === NULL || !class_exists($className)) {
+				unset($registeredNodeTypes[$index]);
+				continue;
+			}
+			$reflection = new \ReflectionClass($className);
+			if ($reflection->implementsInterface(SiteInterface::class)) {
+				unset($registeredNodeTypes[$index]);
+				continue;
+			}
+		}
 		return (array)$registeredNodeTypes;
 	}
 }
