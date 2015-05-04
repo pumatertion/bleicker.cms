90d01fe8ead2103fc4c3a333875824b9f468110b
Carsten Bleicker <carsten@bleicker.de>
2015-05-04 13:32:01 +0200

    Remove leftofer route

--- src/Configuration/Routes.php
+++ src/Configuration/Routes.php
@@ -15,5 +15,4 @@ $router
 	->addRoute('/nodemanager/save/{nodeType}', 'post', new ControllerRouteData(NodeController::class, 'createAction'))
 	->addRoute('/nodemanager/update/{node}', 'post', new ControllerRouteData(NodeController::class, 'updateAction'))
 	->addRoute('/nodemanager/update/{node}', 'patch', new ControllerRouteData(NodeController::class, 'updateAction'))
-	->addRoute('/nodemanager/{node}', 'get', new ControllerRouteData(NodeController::class, 'showAction'))
-	->addRoute('/nodemanager/{node}', 'patch', new ControllerRouteData(NodeController::class, 'updateAction'));
+	->addRoute('/nodemanager/{node}', 'get', new ControllerRouteData(NodeController::class, 'showAction'));
