d864c268c49cf2713041f5e3719222fe2283809c
Carsten Bleicker <carsten@bleicker.de>
2015-05-06 16:47:14 +0200

    Remove Modules Configuration

--- src/Configuration/Modules.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Bleicker\Cms\Controller\NodeController;
-use Bleicker\Registry\Registry;
-
-Registry::set('bleicker.cms.modules.nodeManager.controller', NodeController::class);
-Registry::set('bleicker.cms.modules.nodeManager.endpoints.index', '/manager');
-Registry::set('bleicker.cms.modules.nodeManager.endpoints.show', '/manager/{node}');
