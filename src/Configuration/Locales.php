146f68d180a543f748f7cadedca9974b363711d3
Carsten Bleicker <carsten@bleicker.de>
2015-05-08 08:01:52 +0200

    Register Locale

--- src/Configuration/Locales.php
+++ src/Configuration/Locales.php
@@ -3,5 +3,6 @@
 use Bleicker\Translation\Locale;
 
 Locale::register('Deutsch', 'de', 'DE');
+Locale::register('Schweiz', 'de', 'CH');
 Locale::register('English', 'en', 'GB');
 Locale::register('Français', 'fr', 'FR');
