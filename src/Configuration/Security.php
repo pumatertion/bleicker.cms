94c163a23bfd9135b33f884e0fa9427265e4e20e
Carsten Bleicker <carsten@bleicker.de>
2015-05-11 16:27:16 +0200

    Require bleicker/authentication:2.1

--- src/Configuration/Security.php
+++ src/Configuration/Security.php
@@ -1,19 +1,22 @@
 <?php
 
+use Bleicker\Account\RoleInterface;
+use Bleicker\Authentication\AuthenticationManagerInterface;
 use Bleicker\Cms\Controller\NodeController;
 use Bleicker\Cms\Security\Exception\LoginBoxException;
 use Bleicker\Cms\Security\HttpToken;
 use Bleicker\ObjectManager\ObjectManager;
 use Bleicker\Security\Vote;
-use Bleicker\Session\SessionInterface;
 
 HttpToken::register(HttpToken::class);
 
 Vote::register('SecuredController', function () {
-	/** @var SessionInterface $session */
-	$session = ObjectManager::get(SessionInterface::class);
-	$session->start();
-	if ($session->get(HttpToken::SESSION_KEY) === NULL) {
-		throw new LoginBoxException('Authentication failed.', 1431290565);
+	/** @var AuthenticationManagerInterface $authenticationManager */
+	$authenticationManager = ObjectManager::get(AuthenticationManagerInterface::class);
+	$isAdmin = (boolean)$authenticationManager->getRoles()->filter(function (RoleInterface $role) {
+		return $role->getName() === 'Administrator';
+	})->count();
+	if (!$isAdmin) {
+		throw new LoginBoxException('Administrator privilege required.', 1431290565);
 	}
 }, NodeController::class . '::.*');
--- src/Security/HttpToken.php
+++ src/Security/HttpToken.php
@@ -3,22 +3,21 @@
 namespace Bleicker\Cms\Security;
 
 use Bleicker\Account\Account;
+use Bleicker\Account\AccountInterface;
 use Bleicker\Account\Credential;
 use Bleicker\Framework\ApplicationRequestInterface;
-use Bleicker\Framework\Utility\Arrays;
 use Bleicker\ObjectManager\ObjectManager;
 use Bleicker\Persistence\EntityManagerInterface;
-use Bleicker\Token\AbstractToken;
-use Bleicker\Framework\Http\Request;
+use Bleicker\Token\AbstractSessionToken;
 
 /**
  * Class Token
  *
  * @package Bleicker\Account
  */
-class HttpToken extends AbstractToken {
+class HttpToken extends AbstractSessionToken {
 
-	const USERNAME = 'username', PASSWORD = 'password', SESSION_KEY = 'authenticatedAccount';
+	const USERNAME = 'username', PASSWORD = 'password';
 
 	/**
 	 * @var ApplicationRequestInterface
@@ -37,40 +36,43 @@ class HttpToken extends AbstractToken {
 		$this->entityManager = ObjectManager::get(EntityManagerInterface::class);
 		$this->request = ObjectManager::get(ApplicationRequestInterface::class);
 		$this->entityManager = ObjectManager::get(EntityManagerInterface::class);
-
-		/** @var Request $httpRequest */
-		$httpRequest = $this->request->getMainRequest();
-		$httpRequest->getSession()->start();
-		if($httpRequest->getSession()->has(self::SESSION_KEY)){
-			$this->status = self::AUTHENTICATION_SUCCESS;
-			return $this;
-		}
 		return parent::initialize();
 	}
 
 	/**
-	 * @return void
+	 * @return AccountInterface
 	 */
-	public function injectCredential() {
-		$contents = $this->request->getContents();
-		$username = Arrays::getValueByPath($contents, self::USERNAME);
-		$password = Arrays::getValueByPath($contents, self::PASSWORD);
-		if ($password !== NULL && $username !== NULL) {
-			$this->credential = array(
-				self::USERNAME => $username,
-				self::PASSWORD => $password
-			);
+	public function reconstituteAccountFromSession() {
+		$this->request->getMainRequest()->getSession()->start();
+		$accountId = $this->request->getMainRequest()->getSession()->get($this->getSessionKey());
+		if ($accountId !== NULL) {
+			$queryBuilder = $this->entityManager->createQueryBuilder();
+			$accounts = $queryBuilder->select('a')->from(Account::class, 'a')->where('a.id = :id')
+				->setParameter('id', $accountId)
+				->getQuery()->execute();
+			if (count($accounts) === 1) {
+				return $accounts[0];
+			}
 		}
+		return NULL;
 	}
 
 	/**
-	 * @return boolean
+	 * @return $this
 	 */
-	public function isCredentialValid() {
+	public function injectCredential() {
+		$this->getCredential()->setValue($this->request->getContent(self::PASSWORD));
+	}
+
+	/**
+	 * @return $this
+	 */
+	public function fetchAndSetAccount() {
+		$identity = $this->request->getContent(self::USERNAME);
 		$queryBuilder = $this->entityManager->createQueryBuilder();
-		$accounts = $queryBuilder->select('c')->from(Credential::class, 'c')->leftJoin('c.account', 'a')->where('c.password = :password AND a.username = :username')
-			->setParameter('username', $this->getCredential()[self::USERNAME])
-			->setParameter('password', $this->getCredential()[self::PASSWORD])
+		$accounts = $queryBuilder->select('c')->from(Credential::class, 'c')->leftJoin('c.account', 'a')->where('c.value = :value AND a.identity = :identity')
+			->setParameter('identity', $identity)
+			->setParameter('value', $this->getCredential()->getValue())
 			->getQuery()->execute();
 
 		if (count($accounts) !== 1) {
@@ -81,7 +83,77 @@ class HttpToken extends AbstractToken {
 		$account = $accounts[0];
 
 		$this->request->getMainRequest()->getSession()->start();
-		$this->request->getMainRequest()->getSession()->set(self::SESSION_KEY, $account->getId());
-		return TRUE;
+		$this->request->getMainRequest()->getSession()->set($this->getSessionKey(), $account->getId());
 	}
+
+
+
+//
+//
+//	const USERNAME = 'username', PASSWORD = 'password', SESSION_KEY = 'authenticatedAccount';
+//
+//	/**
+//	 * @var ApplicationRequestInterface
+//	 */
+//	protected $request;
+//
+//	/**
+//	 * @var EntityManagerInterface
+//	 */
+//	protected $entityManager;
+//
+//	/**
+//	 * @return $this
+//	 */
+//	protected function initialize() {
+//		$this->entityManager = ObjectManager::get(EntityManagerInterface::class);
+//		$this->request = ObjectManager::get(ApplicationRequestInterface::class);
+//		$this->entityManager = ObjectManager::get(EntityManagerInterface::class);
+//
+//		/** @var Request $httpRequest */
+//		$httpRequest = $this->request->getMainRequest();
+//		$httpRequest->getSession()->start();
+//		if ($httpRequest->getSession()->has(self::SESSION_KEY)) {
+//			$this->status = self::AUTHENTICATION_SUCCESS;
+//			return $this;
+//		}
+//		return parent::initialize();
+//	}
+//
+//	/**
+//	 * @return void
+//	 */
+//	public function injectCredential() {
+//		$contents = $this->request->getContents();
+//		$username = Arrays::getValueByPath($contents, self::USERNAME);
+//		$password = Arrays::getValueByPath($contents, self::PASSWORD);
+//		if ($password !== NULL && $username !== NULL) {
+//			$this->credential = array(
+//				self::USERNAME => $username,
+//				self::PASSWORD => $password
+//			);
+//		}
+//	}
+//
+//	/**
+//	 * @return boolean
+//	 */
+//	public function isCredentialValid() {
+//		$queryBuilder = $this->entityManager->createQueryBuilder();
+//		$accounts = $queryBuilder->select('c')->from(Credential::class, 'c')->leftJoin('c.account', 'a')->where('c.password = :password AND a.username = :username')
+//			->setParameter('username', $this->getCredential()[self::USERNAME])
+//			->setParameter('password', $this->getCredential()[self::PASSWORD])
+//			->getQuery()->execute();
+//
+//		if (count($accounts) !== 1) {
+//			return FALSE;
+//		}
+//
+//		/** @var Account $account */
+//		$account = $accounts[0];
+//
+//		$this->request->getMainRequest()->getSession()->start();
+//		$this->request->getMainRequest()->getSession()->set(self::SESSION_KEY, $account->getId());
+//		return TRUE;
+//	}
 }
