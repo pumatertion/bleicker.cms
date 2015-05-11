e978481159cb0671f97ee0061e56c7efcd55170d
Carsten Bleicker <carsten@bleicker.de>
2015-05-11 11:59:52 +0200

    Adjust HttpToken

--- src/Configuration/Security.php
+++ src/Configuration/Security.php
@@ -2,8 +2,18 @@
 
 use Bleicker\Cms\Controller\NodeController;
 use Bleicker\Cms\Security\Exception\LoginBoxException;
+use Bleicker\Cms\Security\HttpToken;
+use Bleicker\ObjectManager\ObjectManager;
 use Bleicker\Security\Vote;
+use Bleicker\Session\SessionInterface;
+
+HttpToken::register(HttpToken::class);
 
 Vote::register('SecuredController', function () {
-	throw new LoginBoxException('Please authenticate to get access.', 1431290564);
+	/** @var SessionInterface $session */
+	$session = ObjectManager::get(SessionInterface::class);
+	$session->start();
+	if ($session->get(HttpToken::SESSION_KEY) === NULL) {
+		throw new LoginBoxException('Authentication failed.', 1431290565);
+	}
 }, NodeController::class . '::.*');
---
+++ src/Security/HttpToken.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Bleicker\Cms\Security;
+
+use Bleicker\Account\Account;
+use Bleicker\Account\Credential;
+use Bleicker\Framework\ApplicationRequestInterface;
+use Bleicker\Framework\Utility\Arrays;
+use Bleicker\ObjectManager\ObjectManager;
+use Bleicker\Persistence\EntityManagerInterface;
+use Bleicker\Token\AbstractToken;
+
+/**
+ * Class Token
+ *
+ * @package Bleicker\Account
+ */
+class HttpToken extends AbstractToken {
+
+	const USERNAME = 'username', PASSWORD = 'password', SESSION_KEY = 'authenticatedAccount';
+
+	/**
+	 * @var ApplicationRequestInterface
+	 */
+	protected $request;
+
+	/**
+	 * @var EntityManagerInterface
+	 */
+	protected $entityManager;
+
+	/**
+	 * @return $this
+	 */
+	protected function initialize() {
+		$this->entityManager = ObjectManager::get(EntityManagerInterface::class);
+		$this->request = ObjectManager::get(ApplicationRequestInterface::class);
+		$this->entityManager = ObjectManager::get(EntityManagerInterface::class);
+		return parent::initialize();
+	}
+
+	/**
+	 * @return void
+	 */
+	public function injectCredential() {
+		$contents = $this->request->getContents();
+		$username = Arrays::getValueByPath($contents, self::USERNAME);
+		$password = Arrays::getValueByPath($contents, self::PASSWORD);
+		if ($password !== NULL && $username !== NULL) {
+			$this->credential = array(
+				self::USERNAME => $username,
+				self::PASSWORD => $password
+			);
+		}
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function isCredentialValid() {
+		$queryBuilder = $this->entityManager->createQueryBuilder();
+		$accounts = $queryBuilder->select('c')->from(Credential::class, 'c')->leftJoin('c.account', 'a')->where('c.password = :password AND a.username = :username')
+			->setParameter('username', $this->getCredential()[self::USERNAME])
+			->setParameter('password', $this->getCredential()[self::PASSWORD])
+			->getQuery()->execute();
+
+		if (count($accounts) !== 1) {
+			return FALSE;
+		}
+
+		/** @var Account $account */
+		$account = $accounts[0];
+
+		$this->request->getMainRequest()->getSession()->start();
+		$this->request->getMainRequest()->getSession()->set(self::SESSION_KEY, $account->getId());
+		return TRUE;
+	}
+}
