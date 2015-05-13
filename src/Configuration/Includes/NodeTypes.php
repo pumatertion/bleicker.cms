0c2ddfec58faab56f0a53d1679bce6bd09cafa6f
Carsten Bleicker <carsten@bleicker.de>
2015-05-13 12:21:55 +0200

    Configure Node-Children with Black-/Whitelist

--- src/Configuration/Includes/NodeTypes.php
+++ src/Configuration/Includes/NodeTypes.php
@@ -12,13 +12,13 @@ use Bleicker\NodeTypes\Site;
 use Bleicker\NodeTypes\Text;
 
 /** Register Sites and allow child types */
-Site::register('site', 'Website', 'The root page of a domain', NodeConfiguration::SITE_GROUP, PageNodeInterface::class, ContentNodeInterface::class);
+Site::register('site', 'Website', 'The root page of a domain', NodeConfiguration::SITE_GROUP, [PageNodeInterface::class, ContentNodeInterface::class], [GridElementInterface::class]);
 
 /** Register Pages */
-Page::register('page', 'Page', 'A simple page', NodeConfiguration::PAGE_GROUP, PageNodeInterface::class, ContentNodeInterface::class);
+Page::register('page', 'Page', 'A simple page', NodeConfiguration::PAGE_GROUP, [PageNodeInterface::class, ContentNodeInterface::class], [GridElementInterface::class]);
 
 /** Register Content */
 Headline::register('headline', 'Headline', 'Title and subtitle', NodeConfiguration::CONTENT_GROUP);
 Text::register('text', 'Text', 'Just text', NodeConfiguration::CONTENT_GROUP);
-Grid::register('grid', 'Grid', 'Grid wich can contain grid-elements', NodeConfiguration::CONTENT_GROUP, GridElementInterface::class);
-GridElement::register('gridelement', 'Grid-Element', 'A Grid-Element', NodeConfiguration::CONTENT_GROUP, ContentNodeInterface::class);
+Grid::register('grid', 'Grid', 'Grid wich can contain grid-elements', NodeConfiguration::CONTENT_GROUP, [GridElementInterface::class]);
+GridElement::register('gridelement', 'Grid-Element', 'A Grid-Element', NodeConfiguration::CONTENT_GROUP, [ContentNodeInterface::class], [GridElementInterface::class]);
