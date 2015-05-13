65bde00ba62073301e2c8dba499cb50d985c3b6f
Carsten Bleicker <carsten@bleicker.de>
2015-05-13 17:14:33 +0200

    Introduce before/after callbacks for ApplicationFactory

--- src/Configuration/Before.php
+++ src/Configuration/Before.php
@@ -3,7 +3,6 @@
 include __DIR__ . '/Includes/Secrets.php';
 include __DIR__ . '/Includes/Locales.php';
 include __DIR__ . '/Includes/Cache.php';
-include __DIR__ . '/Includes/Routes.php';
 include __DIR__ . '/Includes/Persistence.php';
 include __DIR__ . '/Includes/TypeConverter.php';
 include __DIR__ . '/Includes/NodeTypes.php';
