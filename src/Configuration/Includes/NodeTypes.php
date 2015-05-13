50346bde5283704496b1c2aeabca8237a2fd816e
Carsten Bleicker <carsten@bleicker.de>
2015-05-13 11:30:59 +0200

    Introduce self registering nodes

--- src/Configuration/Includes/NodeTypes.php
+++ src/Configuration/Includes/NodeTypes.php
@@ -11,20 +11,14 @@ use Bleicker\NodeTypes\Page;
 use Bleicker\NodeTypes\Site;
 use Bleicker\NodeTypes\Text;
 
-/**
- * Register Sites and allow child types
- */
-NodeConfiguration::register(Site::class, 'site', 'Website', 'The root page of a domain', NodeConfiguration::SITE_GROUP, PageNodeInterface::class, ContentNodeInterface::class);
+/** Register Sites and allow child types */
+Site::register('site', 'Website', 'The root page of a domain', NodeConfiguration::SITE_GROUP, PageNodeInterface::class, ContentNodeInterface::class);
 
-/**
- * Register Pages
- */
-NodeConfiguration::register(Page::class, 'page', 'Page', 'A simple page', NodeConfiguration::PAGE_GROUP, PageNodeInterface::class, ContentNodeInterface::class);
+/** Register Pages */
+Page::register('page', 'Page', 'A simple page', NodeConfiguration::PAGE_GROUP, PageNodeInterface::class, ContentNodeInterface::class);
 
-/**
- * Register Content
- */
-NodeConfiguration::register(Headline::class, 'headline', 'Headline', 'Title and subtitle', NodeConfiguration::CONTENT_GROUP);
-NodeConfiguration::register(Text::class, 'text', 'Text', 'Just text', NodeConfiguration::CONTENT_GROUP);
-NodeConfiguration::register(Grid::class, 'grid', 'Grid', 'Grid wich can contain grid-elements', NodeConfiguration::CONTENT_GROUP, GridElementInterface::class);
-NodeConfiguration::register(GridElement::class, 'gridelement', 'Grid-Element', 'A Grid-Element', NodeConfiguration::CONTENT_GROUP, ContentNodeInterface::class);
+/** Register Content */
+Headline::register('headline', 'Headline', 'Title and subtitle', NodeConfiguration::CONTENT_GROUP);
+Text::register('text', 'Text', 'Just text', NodeConfiguration::CONTENT_GROUP);
+Grid::register('grid', 'Grid', 'Grid wich can contain grid-elements', NodeConfiguration::CONTENT_GROUP, GridElementInterface::class);
+GridElement::register('gridelement', 'Grid-Element', 'A Grid-Element', NodeConfiguration::CONTENT_GROUP, ContentNodeInterface::class);
