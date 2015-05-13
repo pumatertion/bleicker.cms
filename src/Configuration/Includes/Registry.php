38605b832d82155f25596d8d42c27ff495a494b8
Carsten Bleicker <carsten@bleicker.de>
2015-05-13 21:18:59 +0200

    Hidden resources folder

--- src/Configuration/Includes/Registry.php
+++ src/Configuration/Includes/Registry.php
@@ -3,17 +3,19 @@
 use Bleicker\Registry\Registry;
 
 /** Doctrine Database Settings */
-Registry::set('doctrine.db.default', ['driver' => 'pdo_sqlite', 'path' => __DIR__ . '/../../../db.sqlite']);
+Registry::set('doctrine.db.default', ['driver' => 'pdo_sqlite', 'path' => __DIR__ . '/../../../.resources/persistence/db.sqlite']);
 
 /** Paths */
 Registry::set('paths.doctrine.schema.nodes', __DIR__ . "/../../../vendor/bleicker/nodes/src/Schema/Persistence");
 Registry::set('paths.doctrine.schema.nodestypes', __DIR__ . "/../../../vendor/bleicker/nodetypes/src/Schema/Persistence");
 Registry::set('paths.doctrine.schema.account', __DIR__ . "/../../../vendor/bleicker/account/src/Schema/Persistence");
-Registry::set('paths.cache.default', __DIR__ . '/../../../resources/cache');
+
 Registry::set('paths.typo3.fluid.templateRootPaths.cms', __DIR__ . '/../../Private/Templates/');
 Registry::set('paths.typo3.fluid.layoutRootPaths.cms', __DIR__ . '/../../Private/Layouts/');
 Registry::set('paths.typo3.fluid.partialRootPaths.cms', __DIR__ . '/../../Private/Partials/');
-Registry::set('paths.resources.default', __DIR__ . '/../../resources');
+
+Registry::set('paths.cache.default', __DIR__ . '/../../../.resources/cache');
+Registry::set('paths.uploads.default', __DIR__ . '/../../../.resources/uploads');
 
 /** Load Local Settings if exists */
 if (file_exists(__DIR__ . '/Registry.local.php')) {
