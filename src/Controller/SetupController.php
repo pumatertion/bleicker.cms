b63af2d5ad16363d3f2861c2bb5674678d7319e9
Carsten Bleicker <carsten@bleicker.de>
2015-05-14 20:53:25 +0200

    Finalize Steps

--- src/Controller/SetupController.php
+++ src/Controller/SetupController.php
@@ -2,9 +2,13 @@
 
 namespace Bleicker\Cms\Controller;
 
+use Bleicker\Account\Account;
+use Bleicker\Account\Credential;
+use Bleicker\Account\Role;
 use Bleicker\Cms\Security\SetupToken;
 use Bleicker\Framework\Controller\AbstractController;
 use Bleicker\Framework\Security\Vote\Exception\ControllerInvokationExceptionInterface;
+use Bleicker\Registry\Registry;
 
 /**
  * Class SetupController
@@ -71,8 +75,33 @@ class SetupController extends AbstractController {
 
 	/**
 	 * @return string
+	 * @todo validate url argument
 	 */
-	public function createDatabaseAction(){
+	public function createDatabaseAction() {
+		$path = Registry::get('paths.root') . '/src/Configuration/Includes/Secrets.local.php';
+		$content = <<<CONTENT
+<?php
+
+use Bleicker\Registry\Registry;
+
+Registry::set('doctrine.db.default.url', '%1\$s');
+CONTENT;
+
+		if (is_file($path)) {
+			unlink($path);
+		}
+
+		file_put_contents($path, sprintf($content, $this->request->getContent('url')));
+
+		$this->redirect('/setup/schema', 307);
+	}
+
+	/**
+	 * @return void
+	 */
+	public function createSchemaAction() {
+		$tool = new \Doctrine\ORM\Tools\SchemaTool($this->entityManager);
+		$tool->createSchema($this->entityManager->getMetadataFactory()->getAllMetadata());
 		$this->redirect('/setup/admin');
 	}
 
@@ -86,7 +115,18 @@ class SetupController extends AbstractController {
 	/**
 	 * @return string
 	 */
-	public function createAdministratorAction(){
+	public function createAdministratorAction() {
+		$account = new Account();
+		$account
+			->setIdentity($this->request->getContent('username'))
+			->addRole(new Role('Administrator'));
+
+		$credentialValue = $this->request->getContent('password');
+		$credential = new Credential($credentialValue, $account);
+
+		$this->entityManager->persist($credential);
+		$this->entityManager->flush();
+
 		$this->redirect('/nodemanager');
 	}
 }
