d2c72a76fcb285b6b157772dd08b3064ec65ee1e
Carsten Bleicker <carsten@bleicker.de>
2015-05-14 18:07:20 +0200

    Setup admin after database creation

--- src/Controller/SetupController.php
+++ src/Controller/SetupController.php
@@ -73,7 +73,7 @@ class SetupController extends AbstractController {
 	 * @return string
 	 */
 	public function createDatabaseAction(){
-		$this->redirect('/setup/database');
+		$this->redirect('/setup/admin');
 	}
 
 	/**
