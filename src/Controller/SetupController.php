4c478e7801fa873b0a1a52adf4feda00624573bf
Carsten Bleicker <carsten@bleicker.de>
2015-05-14 13:51:26 +0200

    Introduce new Actions
    newTokenAction()
    createTokenAction()
    authenticateAction()
    authenticationAction()
    setupAction()

--- src/Controller/SetupController.php
+++ src/Controller/SetupController.php
@@ -3,6 +3,7 @@
 namespace Bleicker\Cms\Controller;
 
 use Bleicker\Framework\Controller\AbstractController;
+use Bleicker\Registry\Registry;
 
 /**
  * Class SetupController
@@ -11,10 +12,55 @@ use Bleicker\Framework\Controller\AbstractController;
  */
 class SetupController extends AbstractController {
 
+	const TOKEN_FILENAME = 'setup.token';
+
+	/**
+	 * @var string
+	 */
+	protected $tokenFile;
+
+	public function __construct() {
+		parent::__construct();
+		$this->tokenFile = Registry::get('paths.tokens.default') . self::TOKEN_FILENAME;
+	}
+
 	/**
 	 * @return string
 	 */
-	public function indexAction() {
+	public function newTokenAction() {
+		if (!file_exists($this->tokenFile)) {
+			return $this->view->render();
+		}
+		$this->redirect('/setup/authenticate');
+	}
+
+	/**
+	 * @return void
+	 */
+	public function createTokenAction() {
+		$password = $this->request->getContent('password');
+		file_put_contents($this->tokenFile, $password);
+		$this->redirect('/setup/authentication');
+	}
+
+	/**
+	 * @return string
+	 */
+	public function authenticationAction() {
+		return $this->view->render();
+	}
+
+	/**
+	 * @return string
+	 */
+	public function authenticateAction() {
+		$this->redirect('/setup');
+	}
+
+	/**
+	 * @return string
+	 */
+	public function setupAction() {
 		return $this->view->render();
 	}
 }
