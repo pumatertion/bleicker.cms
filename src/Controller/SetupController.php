f226c60764e06c542d30a595fb623336f4098e16
Carsten Bleicker <carsten@bleicker.de>
2015-05-14 13:14:55 +0200

    Introduce SetupController

---
+++ src/Controller/SetupController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Bleicker\Cms\Controller;
+
+use Bleicker\Framework\Controller\AbstractController;
+
+/**
+ * Class SetupController
+ *
+ * @package Bleicker\Cms\Controller
+ */
+class SetupController extends AbstractController {
+
+	/**
+	 * @return string
+	 */
+	public function indexAction() {
+		return 'foo';
+	}
+}
