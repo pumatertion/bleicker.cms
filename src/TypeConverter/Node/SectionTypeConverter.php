8b21ad4c260bb7f7b86a54f846bacd8da957ebd1
Carsten Bleicker <carsten@bleicker.de>
2015-05-16 15:07:28 +0200

    Introduce SectionTypeConverter

---
+++ src/TypeConverter/Node/SectionTypeConverter.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Bleicker\Cms\TypeConverter\Node;
+
+use Bleicker\Converter\AbstractTypeConverter;
+use Bleicker\Framework\Utility\Arrays;
+use Bleicker\Nodes\NodeService;
+use Bleicker\Nodes\NodeServiceInterface;
+use Bleicker\NodeTypes\Section;
+use Bleicker\ObjectManager\ObjectManager;
+
+/**
+ * Class SectionTypeConverter
+ *
+ * @package Bleicker\Cms\TypeConverter\Node
+ */
+class SectionTypeConverter extends AbstractTypeConverter {
+
+	/**
+	 * @var NodeServiceInterface
+	 */
+	protected $nodeService;
+
+	public function __construct() {
+		parent::__construct();
+		$this->nodeService = ObjectManager::get(NodeServiceInterface::class, NodeService::class);
+	}
+
+	/**
+	 * @param array $source
+	 * @param string $targetType
+	 * @return boolean
+	 */
+	public static function canConvert($source = NULL, $targetType) {
+		if (is_array($source) && $targetType === Section::class) {
+			return TRUE;
+		}
+		return FALSE;
+	}
+
+	/**
+	 * @param array $source
+	 * @return Section
+	 */
+	public function convert($source) {
+		if ($this->isUpdate($source)) {
+			return $this->getUpdated($source);
+		}
+		return $this->getNew($source);
+	}
+
+	/**
+	 * Returns true if value of path "className.id" not null
+	 *
+	 * @param array $source
+	 * @return boolean
+	 */
+	protected function isUpdate(array $source) {
+		return $this->getIdFromSource($source) !== NULL;
+	}
+
+	/**
+	 * @param array $source
+	 * @return mixed
+	 */
+	protected function getIdFromSource(array $source) {
+		return Arrays::getValueByPath($source, $this->getIdPath());
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function getIdPath() {
+		return 'id';
+	}
+
+	/**
+	 * Returns a new site mapped with source arguments
+	 *
+	 * @param array $source
+	 * @return Section
+	 */
+	protected function getNew(array $source) {
+		$node = new Section();
+		return $node;
+	}
+
+	/**
+	 * Returns an updated site mapped with source arguments
+	 *
+	 * @param array $source
+	 * @return Section
+	 */
+	protected function getUpdated(array $source) {
+		$nodeId = Arrays::getValueByPath($source, $this->getIdPath());
+		Arrays::unsetValueByPath($source, $this->getIdPath());
+		/** @var Section $node */
+		$node = $this->nodeService->get($nodeId);
+		return $node;
+	}
+}
