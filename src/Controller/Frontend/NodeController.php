d683e827530917c0043df6fd6c20c254e67dfddf
Carsten Bleicker <carsten@bleicker.de>
2015-05-22 22:53:11 +0200

    Find Sites by Context

--- src/Controller/Frontend/NodeController.php
+++ src/Controller/Frontend/NodeController.php
@@ -3,6 +3,7 @@
 namespace Bleicker\Cms\Controller\Frontend;
 
 use Bleicker\Cms\Modules\ModuleTrait;
+use Bleicker\Context\ContextInterface;
 use Bleicker\Framework\Controller\AbstractController;
 use Bleicker\Nodes\NodeInterface;
 use Bleicker\Nodes\NodeService;
@@ -30,25 +31,23 @@ class NodeController extends AbstractController {
 	 * @return string
 	 */
 	public function indexAction() {
-		/** @var NodeInterface $node */
-		$sites = $this->nodeService->findSites();
-		$node = $sites->first();
-		$page = $node;
-		return $this->view->assign('node', $node)->assign('page', $page)->assign('sites', $sites)->render();
+		$site = $this->nodeService->findDomainSites($this->context->get(ContextInterface::DOMAIN_CONTEXT))->first();
+		return $this->view->assign('node', $site)->assign('page', $site)->assign('site', $site)->assign('validationException', $this->getValidationException())->render();
 	}
 
 	/**
 	 * @param string $node
 	 * @return string
+	 * @todo make sure only nodes from current domain
 	 */
 	public function showAction($node) {
 		/** @var NodeInterface $node */
 		$node = $this->nodeService->get($node);
 		$page = $this->nodeService->locatePage($node);
+		$site = $this->nodeService->findDomainSites($this->context->get(ContextInterface::DOMAIN_CONTEXT))->first();
 		if ($page === NULL) {
-			$page = $this->nodeService->locateSite($node);
+			$page = $site;
 		}
-		$sites = $this->nodeService->findSites();
-		return $this->view->assign('node', $node)->assign('page', $page)->assign('sites', $sites)->assign('validationException', $this->getValidationException())->render();
+		return $this->view->assign('node', $node)->assign('page', $page)->assign('site', $site)->assign('validationException', $this->getValidationException())->render();
 	}
 }
